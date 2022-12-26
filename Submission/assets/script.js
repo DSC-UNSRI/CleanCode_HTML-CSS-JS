b685cc581ee56ce78c3ef6afeac73045c4ba8e76
Cindy Putri Az Zahra <120231523+cindyyputri@users.noreply.github.com>
2022-12-27 00:12:03 +0700

    commit-file submission cindy

---
+++ Submission/assets/script.js
@@ -0,0 +1,35 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.home-section, .doing-section,
+  .timeline-section, .skills-section, .project-section,
+  .contact-section`, {
+    interval: 200
+})
